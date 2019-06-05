0ec991e8b3c84d0683ee0d4751e972397a2ee65b
Ken Mijime <kenaco666@gmail.com>
2019-06-05 21:42:27 +0200

    feat: Replace docopt with click #19

--- auto_changelog/__main__.py
+++ auto_changelog/__main__.py
@@ -1,43 +1,21 @@
-"""
-Generage a changelog straight from your git commits.
-
-Usage: auto-changelog [options]
-
-Options:
-    -r=REPO --repo=REPO     Path to the repository's root directory [Default: .]
-    -t=TITLE --title=TITLE  The changelog's title [Default: Changelog]
-    -d=DESC --description=DESC
-                            Your project's description
-    -o=OUTFILE --output=OUTFILE
-                            The place to save the generated changelog 
-                            [Default: CHANGELOG.md]
-    -h --help               Print this help text
-    -V --version            Print the version number
-"""
-
 import os
 
-import docopt
+import click
 
 from auto_changelog import generate_changelog
 from auto_changelog.presenter import MarkdownPresenter
 from auto_changelog.repository import GitRepository
-from auto_changelog import __version__
 
 
 def main():
-    args = docopt.docopt(__doc__, version=__version__)
     # Convert the repository name to an absolute path
-    repo = os.path.abspath(args['--repo'])
+    repo = os.path.abspath('.')
 
     repository = GitRepository(repo)
     presenter = MarkdownPresenter()
-    title = args['--title']
-    description = args['--description']
+    title = 'Changelog'
+    description = ''
     changelog = generate_changelog(repository, presenter, title, description)
-
-    with open(args['--output'], 'w') as f:
-        f.write(changelog)
 
 
 if __name__ == "__main__":
