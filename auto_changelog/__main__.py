e877b72f0262d287c3f5084b8b66580176cb7aac
WqyJh <781345688@qq.com>
2019-08-21 17:10:50 +0800

    feat: add --repo option

--- auto_changelog/__main__.py
+++ auto_changelog/__main__.py
@@ -8,13 +8,14 @@ from auto_changelog.repository import GitRepository
 
 
 @click.command()
+@click.option('-r', '--repo', type=click.Path(exists=True), default='.')
 @click.option('-o', '--output', type=click.File('w'), default='CHANGELOG.md')
 @click.option('-u', '--unreleased', is_flag=True, default=False)
 @click.option('--stdout', is_flag=True)
 @click.option('--stopping-commit', help='Stopping commit to use for changelog generation', default='HEAD')
-def main(output, unreleased: bool, stdout: bool, stopping_commit: str):
+def main(repo, output, unreleased: bool, stdout: bool, stopping_commit: str):
     # Convert the repository name to an absolute path
-    repo = os.path.abspath('.')
+    repo = os.path.abspath(repo)
 
     repository = GitRepository(repo, skip_unreleased=not unreleased)
     presenter = MarkdownPresenter()
