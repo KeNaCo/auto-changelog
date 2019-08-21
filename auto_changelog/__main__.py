c5fe6f1c30d9b3e3bad78791bdf0ad5f75f9ef2c
WqyJh <781345688@qq.com>
2019-08-21 17:12:50 +0800

    feat: add --title option

--- auto_changelog/__main__.py
+++ auto_changelog/__main__.py
@@ -9,17 +9,17 @@ from auto_changelog.repository import GitRepository
 
 @click.command()
 @click.option('-r', '--repo', type=click.Path(exists=True), default='.')
+@click.option('-t', '--title', default='Changelog')
 @click.option('-o', '--output', type=click.File('w'), default='CHANGELOG.md')
 @click.option('-u', '--unreleased', is_flag=True, default=False)
 @click.option('--stdout', is_flag=True)
 @click.option('--stopping-commit', help='Stopping commit to use for changelog generation', default='HEAD')
-def main(repo, output, unreleased: bool, stdout: bool, stopping_commit: str):
+def main(repo, title, output, unreleased: bool, stdout: bool, stopping_commit: str):
     # Convert the repository name to an absolute path
     repo = os.path.abspath(repo)
 
     repository = GitRepository(repo, skip_unreleased=not unreleased)
     presenter = MarkdownPresenter()
-    title = 'Changelog'
     description = ''
     changelog = generate_changelog(repository, presenter, title, description, stopping_commit=stopping_commit)
     output.write(changelog)
