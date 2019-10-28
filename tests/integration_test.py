4f489296393adc7e32e4599ca1db5009b4079791
Ken Mijime <kenaco666@gmail.com>
2019-10-28 15:27:45 +0100

    test: Add integration tests #50

---
+++ tests/integration_test.py
@@ -0,0 +1,50 @@
+import os
+import pytest
+import subprocess
+
+from click.testing import CliRunner
+
+from auto_changelog.__main__ import main
+
+
+@pytest.fixture
+def commands():
+    return ["git init -q"]
+
+
+@pytest.fixture
+def test_repo(tmp_path, commands):
+    cwd = os.getcwd()
+    os.chdir(str(tmp_path))
+    for command in commands:
+        subprocess.run(command.split())
+    yield tmp_path
+    os.chdir(cwd)
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+@pytest.fixture
+def open_changelog(test_repo):
+    file = None
+
+    def _open_changelog():
+        nonlocal file
+        file = open("CHANGELOG.md", "r")
+        return file
+
+    yield _open_changelog
+
+    if file:
+        file.close()
+
+
+def test_empty_repo(test_repo, runner, open_changelog):
+    result = runner.invoke(main)
+    assert result.exit_code == 0, result.stderr
+    assert result.output == ""
+    changelog = open_changelog().read()
+    assert changelog == "# Changelog\n"
