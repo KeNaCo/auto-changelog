855f77d5285e46f717c8bbd9acdd868c2eebece4
Ken Mijime <kenaco666@gmail.com>
2019-11-01 21:33:01 +0100

    test: Add --issue-pattern integration test #50

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -147,3 +147,16 @@ def test_option_issue_url(test_repo, runner, open_changelog):
     assert result.output == ""
     changelog = open_changelog().read()
     assert "[#1](issues.custom.com/1)" in changelog
+
+
+@pytest.mark.parametrize(
+    "commands", [["git init -q", "touch file", "git add file", "git commit -m 'feat: Add file PRO-1' -q"]]
+)
+def test_option_issue_pattern(test_repo, runner, open_changelog):
+    result = runner.invoke(
+        main, ["--issue-pattern", r" (\w\w\w-\d+)", "--issue-url", "issues.custom.com/{id}", "--unreleased"]
+    )
+    assert result.exit_code == 0, result.stderr
+    assert result.output == ""
+    changelog = open_changelog().read()
+    assert "[PRO-1](issues.custom.com/PRO-1)" in changelog
